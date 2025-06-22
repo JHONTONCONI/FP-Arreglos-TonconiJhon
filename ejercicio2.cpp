ad4e5aea550b8b78282727fbad2792a02c41bbe7
JHONTONCONI <btaedwin123@gmail.com>
2025-06-22 16:36:11 -0400

    ejercicio 2- eco de la entrada en lineas diferentes, version final

---
+++ ejercicio2.cpp
@@ -0,0 +1,18 @@
+//Programa que haga "eco" de la entrada, poniendo cada palabra en una línea separada.
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+	char palabra[50];
+	cout<<"INGRESE UNA PALABRA(ingrese fin para finalizar el programa)"<<endl;
+	while(cin>>palabra){
+		cout<<endl;
+		if (palabra[0]==f and palabra[1]==i and palabra[2]==n and palabra[3]==0){
+			break;
+		}
+		cout<<palabra<<endl;	
+		cout<<endl;
+	}	
+}
+
